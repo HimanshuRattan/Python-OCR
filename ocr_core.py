3209e0a22efc460192ff72907734029f82667ce7
Himanshu Rattan <35609063+HimanshuRattan@users.noreply.github.com>
2019-08-22 00:09:28 +0530

    init

---
+++ ocr_core.py
@@ -0,0 +1,14 @@
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+
+
+def ocr_core(filename):
+    """
+    This function will handle the core OCR processing of images.
+    """
+    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+    text = pytesseract.image_to_string(Image.open(filename))  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
+    return text  # Then we will print the text in the image
